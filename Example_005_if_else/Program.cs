2779ca7f250d070e696a1f430611807b50dea05b
Artyom.Semashko <simkarik@gmail.com>
2022-12-09 01:32:39 +0300

    Oh hey Mark

---
+++ Example_005_if_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter Username");
+string username = Console.ReadLine();
+if (username.ToLower() == "mark")
+{
+    Console.WriteLine("Oh, hey Mark!");
+}
+else
+{
+    Console.WriteLine("Hello, " + username);
+}
