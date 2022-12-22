bcda5a56711a97b67fc6d0ff5890364b6cd9ee34
Artyom.Semashko <simkarik@gmail.com>
2022-12-23 01:16:59 +0300

    finished 008

--- Example_008/Program.cs
+++ Example_008/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int a1 = 12, b1 = 43, c1 = 53, a2 = 32, b2 = 11, c2 = 44, a3 = 65, b3 = 87, c3 = 63;
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
