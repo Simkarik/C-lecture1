ae9e976b2fa3f26c3c69f888fe5d621e3f6c3fa8
Artyom.Semashko <simkarik@gmail.com>
2022-12-23 01:33:56 +0300

    010

---
+++ Example_010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 32, 53, 22, 55, 123, 56, 34 };
+
+int n = array.Length;
+int find = 22;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
