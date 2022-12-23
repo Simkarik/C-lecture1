0930c2eb927b736cc21914a8a3c03c6071b3727d
Artyom.Semashko <simkarik@gmail.com>
2022-12-23 03:21:09 +0300

    Added seminar 2

---
+++ SemExample_2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10,100);
+int a1 = num/10;
+int a2 = num%10;
+int max = a1;
+if(max < a2)
+{
+    max = a2;
+    Console.WriteLine($"Максимальная цифра в числе {num} --> {max}");
+}
+else if(max > a2)
+{
+    Console.WriteLine($"Максимальная цифра в числе {num} --> {max}");
+}
+else
+{
+    Console.WriteLine($"В числе {num} цифры одинаковы");
+}
---
+++ SemExample_2_5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход два
+//числа и проверяет, является ли одно число квадратом
+//другого.
+
+Console.WriteLine("Введите первое число");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1*n1 == n2 || n2*n2 == n1 )
+{
+    Console.WriteLine($"Квадрат");
+}
+else
+{
+    Console.WriteLine($"Не квадрат");
+}
